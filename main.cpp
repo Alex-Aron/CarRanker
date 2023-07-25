bbacc7abcbfbfd7b44b716ad470f3b68dd377d2b
Alex-Aron <116617272+Alex-Aron@users.noreply.github.com>
2023-07-25 17:14:28 -0400

    Add files via upload
    SFML setup and one button now. Have to give functionality to switch to the second screen. The plan, for now, is to allow the user to search for a car with a DFS vs. a BFS and compare the time it takes to search for the car. Also added arial font to put text on sfml.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <SFML/Graphics.hpp>
 #include "cars.h"
 #include "AdList.h"
 #include "weather.h"
@@ -26,30 +27,23 @@ int main() {
         std::cerr << "Error opening the file." << std::endl;
         return 1;
     }
-
     std::vector<cars> carData;
     std::vector<weather> weatherData;
-
     std::string line;
-    // Read the header row and print the column names
+
     std::getline(carFile, line);
     while (std::getline(carFile, line)) {
         std::vector<std::string> row = splitString(line, ',');
         for(auto & str : row){
             str.erase(std::remove(str.begin(), str.end(), '\"'), str.end());
         }
-        if (row.size() >= 18) { // Ensure the row contains at least 18 columns (a to r)
             cars car(std::stoi(row[0]), std::stoi(row[1]), std::stoi(row[2]), row[3], row[4],
                      true, std::stoi(row[6]), row[7], std::stoi(row[8]), row[9],
                      std::stoi(row[10]), true, row[12], row[13], row[14], std::stoi(row[15]),
                      std::stoi(row[16]), std::stoi(row[17]));
             carData.push_back(car);
-        } else {
-            std::cerr << "Invalid row format: " << line << std::endl;
-        }
     }
 
-    // Printing the content of the CSV file to the console along with column headers
     std::ifstream weatherFile("weather.csv");
     if (!carFile.is_open()) {
         std::cerr << "Error opening the file." << std::endl;
@@ -58,26 +52,69 @@ int main() {
     std::getline(weatherFile, line);
     while (std::getline(weatherFile, line)) {
         std::vector<std::string> row = splitString(line, ',');
-
         for(auto & str : row){
             str.erase(std::remove(str.begin(), str.end(), '\"'), str.end());
         }
-        if (row.size() >= 15) { // Ensure the row contains at least 18 columns (a to r)
-            weather weatherCity(std::stof(row[0]), row[1], std::stoi(row[2]), std::stoi(row[3]), std::stoi(row[4]),
-                    row[5], row[6], row[7], row[9], std::stoi(row[10]), std::stoi(row[11]), std::stoi(row[12]), std::stoi(row[13]), std::stof(row[14]));
+            weather weatherCity(std::stof(row[0]), row[1], std::stoi(row[2]), std::stoi(row[3]), std::stoi(row[4]),row[5], row[6], row[7], row[9], std::stoi(row[10]), std::stoi(row[11]), std::stoi(row[12]), std::stoi(row[13]), std::stof(row[14]));
             weatherData.push_back(weatherCity);
-        } else {
-            std::cerr << "Invalid row format: " << line << std::endl;
-        }
     }
 
-    // Printing the content of the CSV file to the console along with column headers
     for(int i = 0; i < carData.size(); i++){
         for(int j = i + 1; j < carData.size(); j++){
             if(carData[i].getMake() == carData[j].getMake()){
                 adList.insertEdge(carData[i], carData[j]);
             }
         }
+    }
+    // Define the window dimensions
+    const int windowWidth = 540;
+    const int windowHeight = 930;
+
+    // Create the SFML window
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "SFML Portrait Window");
+
+    // Define the button dimensions and position
+    const int buttonWidth = 250;
+    const int buttonHeight = 100;
+    const int buttonX = (windowWidth - buttonWidth) / 2; // Center the button horizontally
+    const int buttonY = (windowHeight * 2) / 3; // Position the button about 1/3 from the bottom
+
+    // Create the button shape and text
+    sf::RectangleShape button(sf::Vector2f(buttonWidth, buttonHeight));
+    button.setPosition(buttonX, buttonY);
+    button.setFillColor(sf::Color::Blue);
+    sf::Font font;
+    font.loadFromFile("arial.ttf");
+    sf::Text buttonText("Find Car", font, 18);
+    buttonText.setPosition(buttonX + (buttonWidth - buttonText.getLocalBounds().width) / 2,
+                           buttonY + (buttonHeight - buttonText.getLocalBounds().height) / 2);
+    buttonText.setFillColor(sf::Color::White);
+    // Main loop
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            // Check for button click
+            if (event.type == sf::Event::MouseButtonReleased) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                    if (button.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
+                        std::cout << "Implement action Later" << std::endl;
+                    }
+                }
+            }
+        }
+
+        // Clear the window
+        window.clear(sf::Color::White);
+
+        // Draw the button
+        window.draw(button);
+        window.draw(buttonText);
+
+        // Display the content
+        window.display();
     }
 
     carFile.close();
