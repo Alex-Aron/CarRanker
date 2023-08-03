05119d642ca87d6f905f94c8b040bbaccb60bae9
Alex-Aron <116617272+Alex-Aron@users.noreply.github.com>
2023-08-02 20:04:26 -0400

    Add files via upload

---
+++ sorts.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include "cars.h"
+#include "weather.h"
+void sortMake(std::vector<cars>& carData, std::vector<cars>& ans, std::string make);
+void mergeHP(std::vector<cars>& carData, int start, int mid, int end);
+void mergeSortHP(std::vector<cars>& carData, int start, int end);
+void mergeSortHorsepower(std::vector<cars>& carData, std::vector<cars>& ans);
+int qPartitionHP(std::vector<cars>& carData, int low, int high);
+void quickSortHP(std::vector<cars>& carData, int low, int high);
+void quickSortHorsepower(std::vector<cars>& carData, std::vector<cars>& ans);
+int qPartitionC(std::vector<cars>& carData, int low, int high);
+void quickSortC(std::vector<cars>& carData, int low, int high);
+void quickSortGasCity(std::vector<cars>& carData, std::vector<cars>& ans);
+void mergeC(std::vector<cars>& carData, int start, int mid, int end);
+void mergeSortC(std::vector<cars>& carData, int start, int end);
+void mergeSortGasCity(std::vector<cars>& carData, std::vector<cars>& ans);
+int qPartitionH(std::vector<cars>& carData, int low, int high);
+void quickSortH(std::vector<cars>& carData, int low, int high);
+void quickSortGasHighway(std::vector<cars>& carData, std::vector<cars>& ans);
+void mergeHW(std::vector<cars>& carData, int start, int mid, int end);
+void mergeSortHW(std::vector<cars>& carData, int start, int end);
+void mergeSortGasHighway(std::vector<cars>& carData, std::vector<cars>& ans);
